a78b6fada5b9d0d5da8409b31ca4089d17a5ec20
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2022-09-02 20:53:30 +0200

    Completed task for which the goal was to change special inputs into other chars

---
+++ Safen-User-Input-Part-I-htmlspecialchars/main.js
@@ -0,0 +1,39 @@
+// Safen User Input Part I - htmlspecialchars
+// You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more) which contains form fields so visitors can send emails or leave a comment on your website with ease. However, with ease comes danger. Every now and then, a hacker visits your website and attempts to compromise it through the use of XSS (Cross Site Scripting). This is done by injecting script tags into the website through form fields which may contain malicious code (e.g. a redirection to a malicious website that steals personal information).
+
+// Mission
+// Your mission is to implement a function that converts the following potentially harmful characters:
+
+// < --> &lt;
+// > --> &gt;
+// " --> &quot;
+// & --> &amp;
+// Good luck :D
+
+// function htmlspecialchars(formData) {
+//   let arr = formData.split("");
+//   arr.forEach((item, i) => {
+//     if (item === "<") {
+//       arr[i] = "&lt;";
+//     } else if (item === ">") {
+//       arr[i] = "&gt;";
+//     } else if (item === '"') {
+//       arr[i] = "&quot;";
+//     } else if (item === "&") {
+//       arr[i] = "&amp;";
+//     }
+//   });
+//   let result = "";
+//   arr.forEach((item) => (result += item));
+//   return result;
+// }
+
+// BEST PRACTICES
+
+function htmlspecialchars(formData) {
+  return formData
+    .replace(/&/g, "&amp;")
+    .replace(/"/g, "&quot;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+}
