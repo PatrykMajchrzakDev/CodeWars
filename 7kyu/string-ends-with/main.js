84dfd1da7fab51e44e6d4ccf46a3cdfbb3204ee3
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2022-09-06 23:01:23 +0200

    'String ends with?' challenge - check if strings end the same

---
+++ 7kyu/string-ends-with/main.js
@@ -0,0 +1,14 @@
+// Complete solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// Examples:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd')  // returns false
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+//SOLUTION WITH SUBSTRING
+
+// function solution(str, ending){
+//     return (str.substring(str.length - ending.length) === ending)
+//   }
