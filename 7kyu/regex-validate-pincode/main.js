617dda5b14170bee9dace4e2d9010c837447083d
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2022-09-07 20:48:55 +0200

    Completed Validate-PinCode -- Check if pin is number and length is either 4 or 6

---
+++ 7kyu/regex-validate-pincode/main.js
@@ -0,0 +1,27 @@
+function validatePIN(pin) {
+  let checkLength = pin.toString().length;
+  if (
+    pin.match(/^[0-9]+$/) != null &&
+    (checkLength === 4 || checkLength === 6)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//ONE-LINER
+const validatePIN2 = (pin) => /^\d{4}$|^\d{6}$/.test(pin);
+
+/* for my future referencing,
+
+  ^ = start of a line, 
+  \d = [0-9], {4} specifies 4 digits, 
+  {6} specifies 6 digits, $ - end of a line
+  
+  first alternative ^\d{4}$ - equivalent to [0-9], matches exactly 4 digits.
+  second alternative ^\d{6}$ - "", matches previous token exactly 6 digits.
+  
+   use RegEx .test to search for these characters stored.
+   
+*/
