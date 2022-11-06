c2bc7a91f99ef491101265ef307bcec267ef2bf1
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2022-11-06 13:34:13 +0100

    Kata to check array and remove needless string directions

---
+++ 5kyu/directions-reduction/main.js
@@ -0,0 +1,40 @@
+// How I crossed a mountainous desert the smart way.
+// The directions given to the man are, for example, the following (depending on the language):
+
+// ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+// or
+// { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+// or
+// [North, South, South, East, West, North, West]
+// You can immediately see that going "NORTH" and immediately "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply:
+
+// ["WEST"]
+
+// Other examples:
+// In ["NORTH", "SOUTH", "EAST", "WEST"], the direction "NORTH" + "SOUTH" is going north and coming back right away.
+
+// The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, therefore, the final result is []
+
+// In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
+
+// Task
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+// See more examples in "Sample Tests:"
+// Notes
+// Not all paths can be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"] is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST" are not directly opposite of each other and can't become such. Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
+
+function dirReduc(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      (arr[i] === "NORTH" && arr[i + 1] === "SOUTH") ||
+      (arr[i] === "SOUTH" && arr[i + 1] === "NORTH") ||
+      (arr[i] === "EAST" && arr[i + 1] === "WEST") ||
+      (arr[i] === "WEST" && arr[i + 1] === "EAST")
+    ) {
+      arr.splice(i, 2);
+      i = -1;
+    }
+  }
+  return arr;
+}
