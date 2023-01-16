e484c29edc4acc75f5445c054b897f6f1526aa01
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-01-16 18:28:06 +0100

    From given array of nums, return even nums

---
+++ 7kyu/js-array-filter/main.js
@@ -0,0 +1,19 @@
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+// describe("Fixed tests", () => {
+//     it('should return an empty array if there are no even numbers', function(){
+//       assert.deepEqual(getEvenNumbers([1,2,3,6,8,10]), [2,6,8,10])
+//       assert.deepEqual(getEvenNumbers([1,2]), [2])
+//       assert.deepEqual(getEvenNumbers([12,14,15]), [12,14])
+//       assert.deepEqual(getEvenNumbers([13,15]), [])
+//       assert.deepEqual(getEvenNumbers([1,3,9]), [])
+//     });
+//   })
+
+function getEvenNumbers(numbersArray) {
+  return numbersArray.filter((item) => item % 2 === 0);
+}
